d156fcda6942de2fe11cef41a5462ee1ade6265a
Justin Welsch <j.welsch@gmail.com>
2014-10-23 13:39:19 -0400

    Updated CodeGenerator

--- CommandLineLib/prebuild.bat
+++ CommandLineLib/prebuild.bat
@@ -2,11 +2,11 @@
 SET ConfigurationName=%~1
 SET SolutionDir=%~2
 SET ProjectDir=%~3
-SET CodeGenVer=1.0.0.46
+SET CodeGenVer=1.0.0.47
 @ECHO ON
-REM "%SolutionDir%CodeGenerator-%CodeGenVer%\CodeGenerator.exe" "%ProjectDir%Code Templates\ValueAttribute.template" "%ProjectDir%Code Templates\ValueAttribute.replace" "%ProjectDir%Generated Code\$<TypeName>$Attribute.cs" -overwrite
-REM "%SolutionDir%CodeGenerator-%CodeGenVer%\CodeGenerator.exe" "%ProjectDir%Code Templates\CompoundAttribute.template" "%ProjectDir%Code Templates\CompoundAttribute.replace" "%ProjectDir%Generated Code\$<TypeName>$CompoundAttribute.cs" -overwrite
-"%SolutionDir%CodeGenerator-%CodeGenVer%\CodeGenerator.exe" "%ProjectDir%Code Templates\RangeValueAttribute.template" "%ProjectDir%Code Templates\RangeValueAttribute.replace" "%ProjectDir%Generated Code\$<TypeName>$RangeValueAttribute.cs" -overwrite
+REM "%SolutionDir%CodeGenerator-%CodeGenVer%\CodeGenerator.exe" "%ProjectDir%Code Templates\ValueAttribute.template" "%ProjectDir%Code Templates\ValueAttribute.replace" "%ProjectDir%Generated Code\$[TypeName]$Attribute.cs" -overwrite
+REM "%SolutionDir%CodeGenerator-%CodeGenVer%\CodeGenerator.exe" "%ProjectDir%Code Templates\CompoundAttribute.template" "%ProjectDir%Code Templates\CompoundAttribute.replace" "%ProjectDir%Generated Code\$[TypeName]$CompoundAttribute.cs" -overwrite
+"%SolutionDir%CodeGenerator-%CodeGenVer%\CodeGenerator.exe" "%ProjectDir%Code Templates\RangeValueAttribute.template" "%ProjectDir%Code Templates\RangeValueAttribute.replace" "%ProjectDir%Generated Code\$[TypeName]$RangeValueAttribute.cs" -overwrite
 @ECHO OFF
 IF /I "%ConfigurationName%" == "Release" (
    @ECHO ON
