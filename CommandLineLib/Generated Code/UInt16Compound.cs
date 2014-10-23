3e49d71ff08447aa572ba42f3372a957d2d63676
Justin Welsch <j.welsch@gmail.com>
2014-10-23 14:13:22 -0400

    Updated CodeGenerator.exe; used new CodeGenerator to create all Compound argument classes in order to consolidate code.

---
+++ CommandLineLib/Generated Code/UInt16Compound.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Reflection;
+
+namespace CommandLineLib
+{
+   public class UInt16Compound : UInt16Value, ICompoundAttribute
+   {
+      private CompoundManager manager;
+
+      public UInt16Compound( string identifier )
+         : base( 0 )
+      {
+         this.manager = new CompoundManager( this, identifier );
+      }
+
+      public new int Ordinal
+      {
+         get { return base.Ordinal; }
+         set { base.Ordinal = value; }
+      }
+
+      #region ISwitchAttribute Members
+
+      public bool CaseSensitive
+      {
+         get { return this.manager.CaseSensitive; }
+         set { this.manager.CaseSensitive = value; }
+      }
+
+      public string Identifier
+      {
+         get { return this.manager.Identifier; }
+      }
+
+      public string[] Aliases
+      {
+         get { return this.manager.Aliases; }
+         set { this.manager.Aliases = value; }
+      }
+
+      public string[] AllIdentifiers()
+      {
+         return this.manager.AllIdentifiers();
+      }
+
+      #endregion
+
+      public override string ShortName
+      {
+         get { return this.manager.ShortName; }
+         set { this.manager.ShortName = value; }
+      }
+
+      public override string UsageText
+      {
+         get { return this.manager.UsageText(); }
+      }
+
+      public override void SetProperty( PropertyInfo property )
+      {
+         this.manager.SetValueName( property.Name );
+         base.SetProperty( property );
+      }
+
+      public override bool MatchArgument( string argument )
+      {
+         return this.manager.MatchArgument( argument );
+      }
+   }
+}
--- CommandLineLib/prebuild.bat
+++ CommandLineLib/prebuild.bat
@@ -2,10 +2,9 @@
 SET ConfigurationName=%~1
 SET SolutionDir=%~2
 SET ProjectDir=%~3
-SET CodeGenVer=1.0.0.47
+SET CodeGenVer=1.0.0.48
 @ECHO ON
-REM "%SolutionDir%CodeGenerator-%CodeGenVer%\CodeGenerator.exe" "%ProjectDir%Code Templates\ValueAttribute.template" "%ProjectDir%Code Templates\ValueAttribute.replace" "%ProjectDir%Generated Code\$[TypeName]$Attribute.cs" -overwrite
-REM "%SolutionDir%CodeGenerator-%CodeGenVer%\CodeGenerator.exe" "%ProjectDir%Code Templates\CompoundAttribute.template" "%ProjectDir%Code Templates\CompoundAttribute.replace" "%ProjectDir%Generated Code\$[TypeName]$CompoundAttribute.cs" -overwrite
+"%SolutionDir%CodeGenerator-%CodeGenVer%\CodeGenerator.exe" "%ProjectDir%Code Templates\CompoundAttribute.template" "%ProjectDir%Code Templates\CompoundAttribute.replace" "%ProjectDir%Generated Code\$[TypeName]$Compound.cs" -overwrite
 "%SolutionDir%CodeGenerator-%CodeGenVer%\CodeGenerator.exe" "%ProjectDir%Code Templates\RangeValueAttribute.template" "%ProjectDir%Code Templates\RangeValueAttribute.replace" "%ProjectDir%Generated Code\$[TypeName]$RangeValueAttribute.cs" -overwrite
 @ECHO OFF
 IF /I "%ConfigurationName%" == "Release" (
