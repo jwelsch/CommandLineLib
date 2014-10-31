07fa8ae10b5410fb94dd5e0298760e849bd878d5
Justin Welsch <j.welsch@gmail.com>
2014-10-31 16:01:27 -0400

    Updating AssemblyVersionIncrement

--- CommandLineLib/prebuild.bat
+++ CommandLineLib/prebuild.bat
@@ -3,11 +3,14 @@ SET ConfigurationName=%~1
 SET SolutionDir=%~2
 SET ProjectDir=%~3
 SET CodeGenVer=1.0.0.48
+SET AviVer=1.0.6.0
 @ECHO ON
 "%SolutionDir%CodeGenerator-%CodeGenVer%\CodeGenerator.exe" "%ProjectDir%Code Templates\CompoundAttribute.template" "%ProjectDir%Code Templates\CompoundAttribute.replace" "%ProjectDir%Generated Code\$[TypeName]$Compound.cs" -overwrite
 "%SolutionDir%CodeGenerator-%CodeGenVer%\CodeGenerator.exe" "%ProjectDir%Code Templates\RangeValueAttribute.template" "%ProjectDir%Code Templates\RangeValueAttribute.replace" "%ProjectDir%Generated Code\$[TypeName]$RangeValueAttribute.cs" -overwrite
 @ECHO OFF
+SET VersionPart=Build
 IF /I "%ConfigurationName%" == "Release" (
-   @ECHO ON
-   "%SolutionDir%AssemblyVersionIncrement-1.0.0.3\AssemblyVersionIncrement.exe" Build "%ProjectDir%Properties\AssemblyInfo.cs"
+   SET VersionPart=Maintenance
 )
+@ECHO ON
+"%SolutionDir%AssemblyVersionIncrement-%AviVer%\AssemblyVersionIncrement.exe" %VersionPart% "%ProjectDir%Properties\AssemblyInfo.cs"
